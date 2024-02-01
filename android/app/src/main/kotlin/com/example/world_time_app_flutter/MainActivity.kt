96d72f19f925ed8a9a43c704c815cee43a5fced2
hassenmrakbenjebahi <123837615+hassenmrakbenjebahi@users.noreply.github.com>
2024-02-01 17:38:13 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/world_time_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.world_time_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:world_time_app_flutter/pages/home.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
